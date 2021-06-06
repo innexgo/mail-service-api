3faaca362e69b3af65e60f4f3b30d79c84dd7567
gpi <gpimpale29@gmail.com>
2021-06-05 19:47:33 -0700

    fun

--- src/client.rs
+++ src/client.rs
@@ -5,46 +5,36 @@ use super::response;
 
 #[derive(Clone)]
 pub struct MailService {
-    client: Client,
-    mail_service_url: String,
+  client: Client,
+  mail_service_url: String,
 }
 
 impl MailService {
-    pub async fn new(mail_service_url: &str) -> Self {
-        MailService {
-            mail_service_url: String::from(mail_service_url),
-            client: Client::new(),
-        }
+  pub async fn new(mail_service_url: &str) -> Self {
+    MailService {
+      mail_service_url: String::from(mail_service_url),
+      client: Client::new(),
     }
+  }
 
-    // sends a mail to the email specified in destination
-    // request_id is the unique id of the request, for internal use
-    // topic is the description of the email for internal use
-    // title is the reciever visible title
-    // content is the content of the message
-    pub async fn mail(
-        &self,
-        request_id: i64,
-        topic: String,
-        destination: String,
-        title: String,
-        content: String,
-    ) -> Result<response::Mail, response::MailError> {
-        let mnr = request::MailNew {
-            topic,
-            destination,
-            title,
-            content,
-        };
-
-        self.client
-            .post(format!("{}/mail/new", self.mail_service_url))
-            .json(&mnr)
-            .send()
-            .await
-            .map_err(|_| response::MailError::Unknown)?
-            .json()
-            .await
-            .map_err(|_| response::MailError::Unknown)?
-    }
+  // sends a mail to the email specified in destination
+  // request_id is the unique id of the request, for internal use
+  // topic is the description of the email for internal use
+  // title is the reciever visible title
+  // content is the content of the message
+  pub async fn mail(
+    &self,
+    props: request::MailNewProps,
+  ) -> Result<response::Mail, response::MailError> {
+    self
+      .client
+      .post(format!("{}/mail/new", self.mail_service_url))
+      .json(&mnr)
+      .send()
+      .await
+      .map_err(|_| response::MailError::Unknown)?
+      .json()
+      .await
+      .map_err(|_| response::MailError::Unknown)?
+  }
 }

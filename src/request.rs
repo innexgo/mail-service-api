5553bdc92a2b5b2f8f9f7294fba311f3ee33227d
gpi <gpimpale29@gmail.com>
2021-06-20 21:36:59 -0700

    removed plain creationTime, as it was redundant, we can use max and min to find it

--- src/request.rs
+++ src/request.rs
@@ -4,24 +4,23 @@ use serde::{Deserialize, Serialize};
 #[derive(Clone, Debug, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct MailNewProps {
-  pub request_id:i64,
-  pub topic:String,
-  pub destination:String,
-  pub title:String,
-  pub content:String,
+  pub request_id: i64,
+  pub topic: String,
+  pub destination: String,
+  pub title: String,
+  pub content: String,
 }
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct MailViewProps {
-    pub mail_id: Option<i64>,
-    pub request_id: Option<i64>,
-    pub creation_time: Option<i64>,
-    pub min_creation_time: Option<i64>,
-    pub max_creation_time: Option<i64>,
-    pub topic: Option<String>,
-    pub destination: Option<String>,
-    pub title: Option<String>,
-    pub offset: Option<i64>,
-    pub count: Option<i64>,
+  pub mail_id: Option<i64>,
+  pub request_id: Option<i64>,
+  pub min_creation_time: Option<i64>,
+  pub max_creation_time: Option<i64>,
+  pub topic: Option<String>,
+  pub destination: Option<String>,
+  pub title: Option<String>,
+  pub offset: Option<i64>,
+  pub count: Option<i64>,
 }
